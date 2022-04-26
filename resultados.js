71ee5c1ae6e61717c3bdef413e25063ec181812d
patricklimp <103951855+patricklimp@users.noreply.github.com>
2022-04-26 18:33:42 -0300

    Add files via upload

---
+++ resultados.js
@@ -0,0 +1,45 @@
+// Zoom in/out: quantidade de clientes que responderam
+function increase1(){
+    document.getElementById("area1").style.width = '250px';
+    document.getElementById("area1").style.height= '250px';
+
+}
+
+function decrease1(){
+    document.getElementById("area1").style.width = '180px';
+    document.getElementById("area1").style.height= '180px';
+}
+
+// Zoom in/out: quantidade de clientes que não responderam
+function increase2(){
+    document.getElementById("area2").style.width = '250px';
+    document.getElementById("area2").style.height= '250px';
+
+}
+
+function decrease2(){
+    document.getElementById("area2").style.width = '180px';
+    document.getElementById("area2").style.height= '180px';
+}
+
+// Zoom in/out: metas da empresa
+function increase3(){
+    document.getElementById("area3").style.width = '250px';
+    document.getElementById("area3").style.height= '250px';
+
+}
+
+function decrease3(){
+    document.getElementById("area3").style.width = '180px';
+    document.getElementById("area3").style.height= '180px';
+}
+
+//Zoom in/out: grafico
+function aumenta1(){
+    document.getElementById("graphics").style.width = '900px';
+    document.getElementById("graphics").style.height= '350px';
+}
+function diminui1(){
+    document.getElementById("graphics").style.width = '750px';
+    document.getElementById("graphics").style.height= '260px';
+}
